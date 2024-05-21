241e8128afd3e8239594fcba8b25f6aa45d16735
StareMe <2207597682@qq.com>
2024-05-21 17:46:52 +0800

    add 医生+科室

---
+++ api/v1/medic.go
@@ -0,0 +1,26 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"service/dao/mysql"
+)
+
+// @Summary 获取医生列表
+// @Tags 医生
+// @Router /api/meidcList [post]
+func GetMedicList(c *gin.Context) {
+	var medic mysql.Medic
+	if err := c.ShouldBindJSON(&medic); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	medicList := mysql.GetMedicList(&medic)
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"msg":  "success",
+		"data": medicList,
+	})
+}
